9db78db93238cdce37a71d27b431d122ecc6f76f
Kim HyunWoo <stories2@naver.com>
2019-11-23 15:34:27 +0900

    구글 맵 패키지 버전 변경, 커스텀 마커 테스트

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AgmCoreModule } from '@agm/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,16 @@ import { ErrorComponent } from './views/error/error.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyB3FYRULDlTqMkdGSfifvCd51fMwLVwCMs'
+      /* apiKey is required, unless you are a 
+      premium customer, in which case you can 
+      use clientId 
+      */
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/views/map/map.component.ts
+++ web/src/app/views/map/map.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  selector: "app-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.css"]
 })
 export class MapComponent implements OnInit {
+  lat: number;
+  lng: number;
+  zoom: number;
+  icon: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.lat = 37.551171;
+    this.lng = 126.9877188;
+    this.zoom = 19;
+    this.icon = {
+      url: 'https://cdn4.iconfinder.com/data/icons/contact-us-19/48/35-512.png',
+      scaledSize: {
+        width: 40,
+        height: 40
+      }
+    };
   }
 
+  ngOnInit() {}
 }
