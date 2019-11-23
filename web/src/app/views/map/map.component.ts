d77c4446acfb486d7f126c6d0f0789cab55c823b
Kim HyunWoo <stories2@naver.com>
2019-11-23 15:48:23 +0900

    문법 교정

--- web/src/app/views/map/map.component.ts
+++ web/src/app/views/map/map.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-map",
-  templateUrl: "./map.component.html",
-  styleUrls: ["./map.component.css"]
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
   lat: number;
